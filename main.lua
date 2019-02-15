96289ce787cd18c82679fe4c2b30c4a57415220d
yamay freitas dos santos <1727567>
2019-02-15 17:43:26 -0300

    inclusao da colisao dos objetos

--- main.lua
+++ main.lua
@@ -33,15 +33,16 @@ carro1.x = display.contentWidth - 1
 carro1.y = display.contentHeight - 85
 carro1.rotation = 90
 carro1.myName = "carro1"
-physics.addBody(carro1, {radius=30, isSensor=true})
+physics.addBody(carro1, {radius=20, isSensor=true})
 carro1:setLinearVelocity (-100,0)
 
 
 local velhinha = display.newImageRect ("velhinha.png", 30, 30)
 velhinha.x = display.contentCenterX
 velhinha.y = display.contentHeight - 30
-physics.addBody( velhinha, { radius=30, isSensor=true})
+physics.addBody( velhinha, { radius=10, isSensor=true})
 velhinha.myName = "velhinha"
+
 
 local function walkingVelhinha( event )
     local velhinha = event.target
@@ -60,7 +61,23 @@ local function walkingVelhinha( event )
     return true
 end
 
-local function onCollision( event ) --incompleto
+
+
+local function restaurarVelhinha() --Incompleto
+    velhinha.isBodyActive = false
+    velhinha.x = display.contentCenterX
+    velhinha.y = display.contentHeight - 30
+
+    
+    transition.to( velhinha, {alpha=1, time=4000,
+        onComplete = function()
+            velhinha.isBodyActive = true
+            died = false
+        end
+    })
+end
+
+local function onCollision( event ) 
 
     if ( event.phase == "began" ) then
 
@@ -70,8 +87,13 @@ local function onCollision( event ) --incompleto
         if ( ( obj1.myName == "velhinha" and obj2.myName == "carro1" ) or
             ( obj1.myName == "carro1" and obj2.myName == "velhinha" ) )
             then
-                display.remove (obj1)
                 display.remove (obj2)
+                local myText = display.newText( "Game Over!", display.contentCenterX, 150, native.systemFont, 60 )
+                myText:setFillColor( 1, 1, 1 )
+
+                --velhinha.alpha = 0
+                --timer.performWithDelay( 1000, restoreShip )
+
         end
     end
 end
@@ -79,4 +101,5 @@ end
 
 
 
+Runtime:addEventListener( "collision", onCollision )
 velhinha:addEventListener("touch", walkingVelhinha)
